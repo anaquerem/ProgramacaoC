41f841d37bea851b7b5ec5386c3e1bff8ad8f9f7
anaquerem <202508889137@alunos.estacio.br>
2025-10-30 01:49:11 +0000

    Declarei as variáveis

---
+++ Sistema_GerenciamentoInventario.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    // Declarar variáveis Produto, Estoque, Valor unitário, Valor total e Quantidade mínima
+    char produtoA[30] = "Produto A";
+    char produtoB[30] = "Produto B";
+
+    Unsigned int estoqueA = 1000;
+    Unsigned int estoqueB = 2000;
+
+    float valorA = 10.50;
+    float valorB = 20.40;
+
+    unsigned int estoqueMinimoA = 500;
+    unsigned int estoqueMinimoB = 2500;
+
+    double valorTotalA;
+    double valorTotalB;
+
+    
+
+
+}
