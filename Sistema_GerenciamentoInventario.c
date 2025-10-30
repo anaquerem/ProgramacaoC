5704d3cfe364bf0ce593566c8bf42c48f759d7d0
anaquerem <202508889137@alunos.estacio.br>
2025-10-30 02:10:12 +0000

    adicinei as informações dos produtos

--- Sistema_GerenciamentoInventario.c
+++ Sistema_GerenciamentoInventario.c
@@ -5,8 +5,8 @@ int main() {
     char produtoA[30] = "Produto A";
     char produtoB[30] = "Produto B";
 
-    Unsigned int estoqueA = 1000;
-    Unsigned int estoqueB = 2000;
+    unsigned int estoqueA = 1000;
+    unsigned int estoqueB = 2000;
 
     float valorA = 10.50;
     float valorB = 20.40;
@@ -17,7 +17,12 @@ int main() {
     double valorTotalA;
     double valorTotalB;
 
-    
+    // Exibir as informações dos produtos
+    printf("Produto: %s tem estoque %u e o valor unitário é R$ %.2f\n", produtoA, estoqueA, valorA);
+    printf("Produto: %s tem estoque %u e o valor unitário é R$ %.2f\n", produtoB, estoqueB, valorB);
+
+    // Comparações com o valor mínimo de estoque
+    printf()
 
 
 }
