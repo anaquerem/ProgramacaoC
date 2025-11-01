332646090d32a7b93fa2071e4c28769980171b71
anaquerem <202508889137@alunos.estacio.br>
2025-11-01 01:50:04 +0000

    Declaração de variáveis

---
+++ if_e_ifelse.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // Declarar variáveis temperatura, umidade e estoque
+    float temperatura, umidade;
+    unsigned int estoque;
+
+    printf("Entre com a temperatura: \n");
+    scanf("%f, &temperatura");
+
+    printf("Entre com a umidade: \n");
+    scanf("%f, &umidade");
+
+    printf("Entre com o estoque: \n");
+    scanf("%u, &estoque");
+    
+
+
+
+}
