c2304771b14533b53bcb43bb72d55d8b4120a0e1
anaquerem <202508889137@alunos.estacio.br>
2025-11-02 02:46:38 +0000

    adiciona estrutura inicial de matriz

---
+++ vetores_e_matrizes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int index;
+
+    char * nomesAlunos[3][3] =  {
+        {"Aluno 0", "Pt: 30", "Mat: 90"},
+        {"Aluno 1", "Pt: 60", "Mat: 60"},
+        {"Aluno 2", "Pt: 90", "Mat: 30"}
+    };
+
+
+
+    return 0;
+}
