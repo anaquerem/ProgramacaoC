16a621baf9862140c9f5025be9c64c0718a54c13
anaquerem <202508889137@alunos.estacio.br>
2025-11-02 02:47:02 +0000

    adiciona funcionalidade para exibir notas dos alunos

--- vetores_e_matrizes.c
+++ vetores_e_matrizes.c
@@ -10,7 +10,14 @@ int main() {
         {"Aluno 2", "Pt: 90", "Mat: 30"}
     };
 
+    printf("Digite o nome do aluno que queira ver as notas... \n");
+    printf("Para o aluno 0, digite 0 \n");
+    printf("Para o aluno 1, digite 1 \n");
+    printf("Para o aluno 2, digite 2 \n");
 
+    scanf("%d", &index);
+
+    printf("A nota do aluno %s são: %s, %s ... \n ", nomesAlunos[index][0], nomesAlunos[index][1], nomesAlunos[index][2]);
 
     return 0;
 }
