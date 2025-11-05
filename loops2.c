cc7667aee6066e283931137a514c5087437ed46a
anaquerem <202508889137@alunos.estacio.br>
2025-11-05 22:33:40 +0000

    criação de condicional if-else

--- loops2.c
+++ loops2.c
@@ -6,8 +6,14 @@ int main(){
 
     do {
 
-        printf("Digite um número par para sair do programa... \n");
+        printf("Digite um número par para sair do programa...");
         scantf("%d", &numero);
+
+        if(numero % 2 == 0){
+            printf("%d é par!", numero);
+        } else {
+            printf("%d é impar!", numero);
+        }
 
     } while (numero % 2 != 0);
 
